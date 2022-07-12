d4586a9e4505cb7b930594e4badf8447f1614b82
Nipun Chamikara <nipunchamikara@yahoo.com>
2022-07-12 23:06:35 +0530

    added animations

--- static/js/index.js
+++ static/js/index.js
@@ -3,3 +3,13 @@ window.addEventListener("scroll", () => {
   if (window.scrollY > 10) navbar.classList.add("scroll");
   else navbar.classList.remove("scroll");
 });
+
+const io = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) entry.target.classList.add("animate");
+    else entry.target.classList.remove("animate");
+  });
+});
+[...document.getElementsByClassName("fade-in")].forEach((element) => {
+  io.observe(element);
+});
